83ff2f1929ff5d65381dd691363350eb5c5b1ac1
José María <josem.velez.gonzalez@gmail.com>
2024-11-08 13:55:33 +0100

    arreglado el buscador y la fomar de ver productos

--- src/pages/productos/productos.component.ts
+++ src/pages/productos/productos.component.ts
@@ -6,12 +6,13 @@ import { ProductoService } from '../../core/servicios/productos/serv-productos';
 import { CommonModule } from '@angular/common';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
   imports: [
-    CommonModule,ReactiveFormsModule
+    CommonModule, ReactiveFormsModule
   ],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss',
@@ -19,31 +20,46 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class ProductosComponent implements OnInit {
   filteredProducts: Producto[] = [];
-   searchTerm = new FormControl('');
+  searchTerm = new FormControl('');
   productos: Producto[] = []
   @Input() id_categoria?: number;
   private productoService = inject(ProductoService);
+  private route = inject(ActivatedRoute);
 
   constructor() { }
 
 
   ngOnInit() {
-     this.cargarProductos();
-      this.searchTerm.valueChanges.pipe(
-        debounceTime(500),
-         distinctUntilChanged(),
-          switchMap(term => this.productoService.searchProducts(term||''))
-        ).subscribe(products => {
-           this.filteredProducts = products;
-            console.log('Productos filtrados:', JSON.stringify(products, null, 2));
-           });
-           }
+    // Obtener el parámetro de la ruta para la categoría
+    this.route.params.subscribe(params => {
+      this.id_categoria = +params['idCategoria'];
+      this.cargarProductos();
+    });
+    this.searchTerm.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(term => this.productoService.searchProducts(term || ''))
+    ).subscribe(products => {
+      this.filteredProducts = products;
+      console.log('Productos filtrados:', JSON.stringify(products, null, 2));
+      
+    });
+
+
+  }
 
   cargarProductos() {
     if (this.id_categoria) {
       this.productoService.getProductosPorCategoria(this.id_categoria).subscribe(
         (productos: Producto[]) => {
-           this.productos = productos; this.filteredProducts = productos; });
+          this.productos = productos; this.filteredProducts = productos;
+        });
+    }
+  }
+  agregarAlCarrito(producto: Producto) { // Lógica para añadir el producto al carrito 
+    console.log('Producto añadido al carrito:', producto);
+    // Aquí puedes añadir la lógica para agregar al carrito, como una llamada a un servicio de carrito }
   }
 }
-}
+
+ 
