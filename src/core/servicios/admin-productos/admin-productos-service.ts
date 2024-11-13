275dee930ac57756a138e8c41998b1e17532dc58
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-11-13 13:44:01 +0100

    Primera Fase del Panel Admin terminado

--- src/core/servicios/admin-productos/admin-productos-service.ts
+++ src/core/servicios/admin-productos/admin-productos-service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Producto } from '../../producto/producto';
-import {Categoria}from '../../categoria/categoria'
+import { Categoria } from '../../categoria/categoria';
 import { CATEGORIAS_MOCK } from '../../../mocks/categoria-mock';
-import {PRODUCTOS_MOCK} from '../../../mocks/productos-mock';
+import { PRODUCTOS_MOCK } from '../../../mocks/productos-mock';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminProductosComponentService {
+  private categoriasSubject: BehaviorSubject<Categoria[]> = new BehaviorSubject<Categoria[]>(CATEGORIAS_MOCK);
+  private productosSubject: BehaviorSubject<Producto[]> = new BehaviorSubject<Producto[]>(PRODUCTOS_MOCK);
 
-
+  constructor() {}
+  
+  getCategorias(): Observable<Categoria[]> {
+    return this.categoriasSubject.asObservable();
+  }
+  getProductos(): Observable<Producto[]> {
+    return this.productosSubject.asObservable();
+  }
+  agregarProducto(producto: Producto) {
+    const productosActuales = this.productosSubject.value;
+    this.productosSubject.next([...productosActuales, producto]);
+  }
 }
--- src/pages/admin-productos/admin-productos.component.ts
+++ src/pages/admin-productos/admin-productos.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AdminProductosComponentService } from '../../core/servicios/admin-productos/admin-productos-service';
+import { Producto } from '../../core/producto/producto';
+import { Categoria } from '../../core/categoria/categoria';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-productos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-productos.component.html',
-  styleUrl: './admin-productos.component.scss'
+  styleUrl: './admin-productos.component.scss',
 })
-export class AdminProductosComponent {
+export class AdminProductosComponent implements OnInit {
+  categorias: Categoria[] = [];
+  articulosFiltrados: Producto[] = [];
+  categoriaSeleccionada: number | null = null;
+  private adminProductosService = inject(AdminProductosComponentService);
 
+  ngOnInit(): void {
+    this.adminProductosService.getCategorias().subscribe((categorias) => {
+      this.categorias = categorias;
+    });
+    this.articulosFiltrados = [];
+  }
+
+  filtrarArticulos(event: Event) {
+    const seleccion = (event.target as HTMLSelectElement).value;
+    if (seleccion !== '') {
+      this.categoriaSeleccionada = parseInt(seleccion, 10);
+      this.adminProductosService.getProductos().subscribe((productos) => {
+        this.articulosFiltrados = productos.filter(
+          (articulo) => articulo.id_categoria === this.categoriaSeleccionada
+        );
+      });
+    } else {
+      this.categoriaSeleccionada = null;
+      this.articulosFiltrados = [];
+    }
+  }
+  agregarProducto(producto: Producto) {
+    this.adminProductosService.agregarProducto(producto);
+  }
 }
