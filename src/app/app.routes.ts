2194fbc7d9153028a0ab3411e83cc31336fd41a7
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-11-20 17:34:04 +0100

    imagenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,16 +10,19 @@ import { AdminEditarProductosComponent } from '../pages/admin-editar-productos/a
 
 
 export const routes: Routes = [
-    {
+  /**
+   *   {
         path:'',
         component:PanelControlAdminComponent
 
     },
-/**{
+   * 
+   */
+{
         path:'',
         component:InicioComponent
 
-    }, */
+    }, 
     
     {
         path:'login',
