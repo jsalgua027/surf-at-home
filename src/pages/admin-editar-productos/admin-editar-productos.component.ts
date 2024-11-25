fe3922b0c0f8b725a1feb79787f3b077ad97b8aa
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-11-25 19:52:16 +0100

    add boton para volver al panel de control del admin

--- src/pages/admin-editar-productos/admin-editar-productos.component.ts
+++ src/pages/admin-editar-productos/admin-editar-productos.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdminEditarProductosService } from '../../core/servicios/admin-editar-productos/admin-editar-productos-service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-editar-productos',
@@ -15,12 +16,17 @@ import { CommonModule } from '@angular/common';
 export class AdminEditarProductosComponent {
 private editarServicios=inject(AdminEditarProductosService); // injecto los servicios
 idProducto:string="";
-private router=inject(ActivatedRoute);// para rediriguir al componente que quiero
+private router=inject(ActivatedRoute);
+private rutas=inject(Router);
 
 ngOnInit(): void {
    this.idProducto = this.router.snapshot.paramMap.get('id')!;// recojo de la ruta el id
     console.log("ID del producto a editar: ", this.idProducto);
      // Aquí puedes usar el servicio para obtener detalles del producto y cargarlo en el formulario }
+}
+
+volver(){
+  this.rutas.navigate(['']);
 }
 
 }
